022992d2cf37e8314b89f667446547b9319b2e57
nathanganesh <deeznutsgottem6969@gmail.com>
2021-08-24 15:48:20 +0200

    first commit

---
+++ stockmarketbackend/src/main/java/com/example/stockmarketbackend/StockmarketbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockmarketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmarketbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockmarketbackendApplication.class, args);
+    }
+
+}
