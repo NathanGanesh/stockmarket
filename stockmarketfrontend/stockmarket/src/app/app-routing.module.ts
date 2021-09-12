07784522690c9d81a2dc29cce3c5fc6622c780ba
nathanganesh <deeznutsgottem6969@gmail.com>
2021-09-13 01:04:35 +0200

    com

--- stockmarketfrontend/stockmarket/src/app/app-routing.module.ts
+++ stockmarketfrontend/stockmarket/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthComponent} from "./auth/auth.component";
 
-const routes: Routes = [{path:'', loadChildren:() => import("./home/home.module").then(m => m.HomeModule)}];
+const routes: Routes = [{path:'', loadChildren:() => import("./home/home.module").then(m => m.HomeModule)}, {path:'login', component:AuthComponent},{path:'register', component:AuthComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- stockmarketfrontend/stockmarket/src/app/app.module.ts
+++ stockmarketfrontend/stockmarket/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MaterialModule} from "./material/material.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthComponent } from './auth/auth.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
